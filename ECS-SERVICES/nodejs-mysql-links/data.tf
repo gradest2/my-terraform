fd292b4c91e34c093a38d6ced85451be310956f5
gradest2 <niamatt@yandex.ru>
2022-08-20 09:21:56 +0200

    fix key prefix

--- ECS-SERVICES/nodejs-mysql-links/data.tf
+++ ECS-SERVICES/nodejs-mysql-links/data.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "ecs-cluster" {
   backend = "s3"
   config = {
     bucket               = "mgerasimov.terraform.backend"
-    workspace_key_prefix = "terraform/infra/ECS-CLUSTER-BEST"
+    workspace_key_prefix = "terraform/infra/ECS-CLUSTER/ECS-CLUSTER-BEST"
     key                  = "terraform.tfstate"
     region               = "eu-central-1"
     profile              = "aws_profile"
